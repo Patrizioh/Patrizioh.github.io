a91da59344f2d1b58f338baf9efb15a2b1f1e5ff
Patrizioh <148588773+Patrizioh@users.noreply.github.com>
2023-10-20 17:58:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-function computeProbability(inputs) {
-    let coefficients = {
-        "Knee Surgery": -0.41,
-        "Age": -0.06,
-        "Marital Status": {
-            "Single": 0,
-            "Married": 0.59,
-            "Divorced": -0.45,
-            "Widowed": 0.05
-        },
-        // Add other coefficients similarly
-    };
-    
-    let z = 0;
-    z += coefficients["Knee Surgery"] * inputs["Knee Surgery"];
-    z += coefficients["Age"] * inputs["Age"];
-    z += coefficients["Marital Status"][inputs["Marital Status"]];
-    // ... (other input calculations here)
-    
-    return 1 / (1 + Math.exp(-z));
-}
+document.getElementById('calculate').addEventListener('click', function() {
+    // Get values from each input
+    let intercept = parseFloat(document.getElementById('intercept').value);
+    // Repeat for other inputs, like:
+    // let previousJointReplacement = parseFloat(document.getElementById('previousJointReplacement').value);
+    // ...
 
-function calculateProbability() {
-    let userInputs = {
-        "Knee Surgery": parseFloat(document.getElementById("KneeSurgery").value),
-        "Age": parseFloat(document.getElementById("Age").value),
-        "Marital Status": document.getElementById("MaritalStatus").value,
-        // Fetch other inputs similarly
-    };
-    let result = computeProbability(userInputs);
-    document.getElementById("resultValue").innerText = result.toFixed(4); // Display the result with 4 decimal places
-}
+    // Calculate the result using the formula
+    let result = intercept; 
+    // Add the multiplication for other inputs, like:
+    // result += previousJointReplacement * 0.042377;
+    // ...
+
+    // Display the result
+    document.getElementById('result').textContent = result.toFixed(2);
+});
