1cfd234c315f30bf2ca362ed28861eb31a6af5a2
Patrizioh <148588773+Patrizioh@users.noreply.github.com>
2023-10-21 00:39:18 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 const coefficients = {
-    intercept: 2.495213,  // Changed the sign for demonstration
-    procedureAge: -0.004224,
+    intercept: 2.495213,
+    procedureAge: -0.04224,
     CCI: -0.20137,
     sex: -0.3021,
     typeOfSurgery: -0.32406,
@@ -13,6 +13,7 @@ const categoricalValues = {};
 
 function setCategory(category, value) {
     categoricalValues[category] = value;
+    // Highlight the selected button
     const categoryButtons = document.querySelectorAll(`.category.${category} button`);
     categoryButtons.forEach(button => button.classList.remove('selected'));
     const clickedButton = document.querySelector(`.category.${category} button[data-value="${value}"]`);
@@ -20,30 +21,16 @@ function setCategory(category, value) {
     calculateLogisticRegression();
 }
 
-function resetCalculator() {
-    document.getElementById('logisticRegressionForm').reset();
-    const categoryButtons = document.querySelectorAll('.category button');
-    categoryButtons.forEach(button => button.classList.remove('selected'));
-    document.getElementById('result').innerHTML = "Probability of Outcome: ";
-    document.getElementById('recommendation').innerHTML = "";
-    document.getElementById('recommendation').className = '';
-}
-
 function calculateLogisticRegression() {
     const procedureAge = parseFloat(document.getElementById('procedureAge').value);
     const CCI = parseFloat(document.getElementById('CCI').value);
     const ASAScore = parseFloat(document.getElementById('ASAScore').value);
 
-    if (isNaN(procedureAge) || isNaN(CCI) || isNaN(ASAScore)) {
-        document.getElementById('probabilityPercent').innerHTML = '';
-        document.getElementById('recommendation').innerHTML = '';
-        return;
-    }
-
     let linearCombination = coefficients.intercept;
     for (const category in categoricalValues) {
         linearCombination += coefficients[category] * categoricalValues[category];
     }
+
     linearCombination += coefficients.procedureAge * procedureAge;
     linearCombination += coefficients.CCI * CCI;
     linearCombination += coefficients.ASAScore * ASAScore;
@@ -51,21 +38,29 @@ function calculateLogisticRegression() {
     const probability = 1 / (1 + Math.exp(-linearCombination));
     const probabilityPercent = (probability * 100).toFixed(2);
 
-    document.getElementById('probabilityPercent').textContent = probabilityPercent;
-
+    const probabilityPercentElement = document.getElementById('probabilityPercent');
     const recommendationElement = document.getElementById('recommendation');
-    if (probability >= 0.8) {
-        recommendationElement.textContent = 'Recommendation: Surgery Center';
-        recommendationElement.className = 'surgery-center';
+
+    if (isNaN(probability)) {
+        probabilityPercentElement.textContent = '';
+        recommendationElement.textContent = 'Please fill in all required fields';
+        recommendationElement.style.color = 'blue';
     } else {
-        recommendationElement.textContent = 'Recommendation: Hospital';
-        recommendationElement.className = 'hospital';
+        probabilityPercentElement.textContent = probabilityPercent;
+        if (probability >= 0.8) {
+            recommendationElement.textContent = 'Recommendation: Surgery Center';
+            recommendationElement.style.color = 'green';
+        } else {
+            recommendationElement.textContent = 'Recommendation: Hospital';
+            recommendationElement.style.color = 'red';
+        }
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const inputFields = document.querySelectorAll('input[type="number"]');
-    inputFields.forEach((input) => {
-        input.addEventListener('input', calculateLogisticRegression);
-    });
+const inputFields = document.querySelectorAll('input[type="number"]');
+inputFields.forEach((input) => {
+    input.addEventListener('input', calculateLogisticRegression);
 });
+
+// Run the initial calculation
+calculateLogisticRegression();
